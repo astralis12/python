7f62f45ddefaa0a5a19857ebef8a6f4f89bf4683
Aristo.Davino <albertusdavino@gmail.com>
2020-07-14 18:43:50 +0700

    random ajg

--- criminal_dict.py
+++ criminal_dict.py
@@ -1,76 +1,82 @@
 import time
-kode = input('masukan passwordnya :')
-while kode != '191919':
-    kode = input ('password?')
 
-print('--------------------')
-print('fbi most wanted list')
-print('--------------------')
-print('')
-time.sleep(0.5)
-print('LOADING')
-time.sleep(0.5)
-print('LOADING.')
-time.sleep(0.5)
-print('LOADING..')
-time.sleep(0.5)
-print('LOADING...')
-time.sleep(0.5)
-print('LOADING')
-time.sleep(0.5)
-print('LOADING.')
-time.sleep(0.5)
-print('LOADING..')
-time.sleep(0.5)
-print('LOADING...')
+kode = input("masukan passwordnya :")
+while kode != "191919":
+    kode = input("password?")
 
-#asedekon
+print("--------------------")
+print("fbi most wanted list")
+print("--------------------")
+print("")
+time.sleep(0.5)
+print("LOADING")
+time.sleep(0.5)
+print("LOADING.")
+time.sleep(0.5)
+print("LOADING..")
+time.sleep(0.5)
+print("LOADING...")
+time.sleep(0.5)
+print("LOADING")
+time.sleep(0.5)
+print("LOADING.")
+time.sleep(0.5)
+print("LOADING..")
+time.sleep(0.5)
+print("LOADING...")
+
+# asedekon
+# x/y
 
 time.sleep(0.5)
-print('LOADING')
+print("LOADING")
 time.sleep(0.5)
-print('LOADING.')
+print("LOADING.")
 time.sleep(0.5)
-print('LOADING..')
+print("LOADING..")
 time.sleep(0.5)
-print('LOADING...')
+print("LOADING...")
 
 
-
-print('')
-print ('4', 'identified')
-print('')
+print("")
+print("4", "identified")
+print("")
 time.sleep(3)
 
 criminal_lists = {}
 
-def BILL(name,age,crime): 
-    print(f'name:{name}' )
-    print(f'age: {age}')
-    print(f'crime: {crime}')
 
-BILL('bill',27,'marijuana')
-
-def JUAN(name,age,crime): 
-    print(f'name:{name}' )
-    print(f'age: {age}')
-    print(f'crime: {crime}')
-
-JUAN('JUAN',31,'PHEDOPHILE')
+def BILL(name, age, crime):
+    print(f"name:{name}")
+    print(f"age: {age}")
+    print(f"crime: {crime}")
 
 
-def JEFF(name,age,crime): 
-    print(f'name:{name}' )
-    print(f'age: {age}')
-    print(f'crime: {crime}')
-
-JEFF('JEFF',24,'SLAVE')
+BILL("bill", 27, "marijuana")
 
 
-def JACK(name,age,crime): 
-    print(f'name:{name}' )
-    print(f'age: {age}')
-    print(f'crime: {crime}')
+def JUAN(name, age, crime):
+    print(f"name:{name}")
+    print(f"age: {age}")
+    print(f"crime: {crime}")
 
-JACK('JACK',29,'robbery')
+JUAN("JUAN", 31, "PHEDOPHILE")
+
+
+def JEFF(name, age, crime):
+    print(f"name:{name}")
+    print(f"age: {age}")
+    print(f"crime: {crime}")
+
+
+JEFF("JEFF", 24, "SLAVE")
+
+
+def JACK(name, age, crime):
+    print(f"name:{name}")
+    print(f"age: {age}")
+    print(f"crime: {crime}")
+
+
+JACK("JACK", 29, "robbery")
 
